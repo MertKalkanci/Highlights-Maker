234d1af88b01c50577c9ad3b128cfdfdd0bbf7fc
Mert Kalkancı <mertkalkanci05@hotmail.com>
2023-11-30 15:04:22 +0300

    .

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from youtube import download
 LINK="https://www.youtube.com/watch?v=3ryID_SwU5E"
 
     
-download(LINK,"temp/3ryID_SwU5E.mp4")
-highlight("temp/3ryID_SwU5E.mp4")
+file_path = download(LINK,"temp")
+highlight(file_path)
